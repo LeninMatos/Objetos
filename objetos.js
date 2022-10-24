81b5baf223a9b189ecd98cd9bb4adadf7727a136
LeninMatos <leninmatos@gmail.com>
2022-10-24 10:04:05 -0300

    novos arquivos, objetos.js e print-infos.js

---
+++ objetos.js
@@ -0,0 +1,10 @@
+// array 
+const listaDeCPFs = ["3654635464", "351715154", "971845184"]
+
+//objetos
+const cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: "1674347321.64",
+    email: "andre@email.com"
+} 
---
+++ print-infos.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: "167434732164",
+    email: "andre@email.com"
+}
+
+//console.log(cliente.nome)
+//console.log(cliente.idade)
+//console.log(cliente.cpf)
+//console.log(cliente.email)
+
+console.log(`O nome do cliente é ${cliente.nome} e ele tem ${cliente.idade} anos, inscrito sob o CPF ${cliente.cpf} e possui o e-mail ${cliente.email}.`)
+console.log('O nome do cliente é ' + cliente.nome + ' e ele tem ' + cliente.idade + ' anos, inscrito sob o CPF ' + cliente.cpf + ' e possui o e-mail ' + cliente.email + '.')
