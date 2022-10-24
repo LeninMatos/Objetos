fc543ee646e014dcaf3a4775a070113ec3bd1a1e
LeninMatos <leninmatos@gmail.com>
2022-10-24 12:36:46 -0300

    novo arquivo acessar array em uma chave

---
+++ acessar-telefones.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: "167434732164",
+    email: "andre@email.com",
+    fones: ["5516997712838", "551999830952"]
+}
+
+cliente.fones.forEach(fone => console.log(fone))
+
